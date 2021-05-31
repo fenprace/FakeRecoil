9e8c3658ce5cb159fa0d13d4e872559bb90da98e
Zhuo FENG <968767+fenprace@users.noreply.github.com>
2021-05-31 21:22:58 +1000

    setup jest tests

---
+++ src/__tests__/RecoilRoot.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { RecoilRoot } from '../index'
+
+it('renders correctly with children', () => {
+  const tree = renderer.create(<RecoilRoot>FakeRecoil</RecoilRoot>).toJSON()
+  expect(tree).toMatchInlineSnapshot(`"FakeRecoil"`)
+})
